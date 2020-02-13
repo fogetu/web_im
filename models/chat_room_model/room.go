568853c707cd476fe0f23748c5a14e154d6c70bf
涂峰旗 <185441127@qq.com>
2020-02-13 16:36:27 +0800

    feat:修改ROOM HEAD

--- models/chat_room_model/room.go
+++ models/chat_room_model/room.go
@@ -13,16 +13,16 @@ var RoomBaseMap = make(map[int32]*RoomBaseModel)
 
 func init() {
 	// mock data
-	RoomBaseMap[1] = &RoomBaseModel{1, "room_1", "https://static-upload.local.com/amodvis/static/image/c7/93/54/c793540262a2156d68d10d427a594a02.png"}
-	RoomBaseMap[2] = &RoomBaseModel{2, "room_2", "https://static-upload.local.com/amodvis/static/image/92/87/35/928735f507fbc8e411a471210212d028.jpg"}
-	RoomBaseMap[3] = &RoomBaseModel{3, "room_3", "https://static-upload.local.com/amodvis/static/image/c7/93/54/c793540262a2156d68d10d427a594a02.png"}
-	RoomBaseMap[4] = &RoomBaseModel{4, "room_4", "https://static-upload.local.com/amodvis/static/image/c7/93/54/c793540262a2156d68d10d427a594a02.png"}
-	RoomBaseMap[5] = &RoomBaseModel{5, "room_5", "https://static-upload.local.com/amodvis/static/image/c7/93/54/c793540262a2156d68d10d427a594a02.png"}
-	RoomBaseMap[6] = &RoomBaseModel{6, "room_6", "https://static-upload.local.com/amodvis/static/image/92/87/35/928735f507fbc8e411a471210212d028.jpg"}
-	RoomBaseMap[7] = &RoomBaseModel{7, "room_7", "https://static-upload.local.com/amodvis/static/image/92/87/35/928735f507fbc8e411a471210212d028.jpg"}
-	RoomBaseMap[8] = &RoomBaseModel{8, "room_8", "https://static-upload.local.com/amodvis/static/image/c7/93/54/c793540262a2156d68d10d427a594a02.png"}
-	RoomBaseMap[9] = &RoomBaseModel{9, "room_9", "https://static-upload.local.com/amodvis/static/image/92/87/35/928735f507fbc8e411a471210212d028.jpg"}
-	RoomBaseMap[10] = &RoomBaseModel{10, "room_10", "https://static-upload.local.com/amodvis/static/image/92/87/35/928735f507fbc8e411a471210212d028.jpg"}
+	RoomBaseMap[1] = &RoomBaseModel{1, "room_1", "http://106.54.93.177:9091/amodvis/static/image/27/a0/a3/27a0a33aeac4e3b4b8b59a43edb34057.jpeg"}
+	RoomBaseMap[2] = &RoomBaseModel{2, "room_2", "http://106.54.93.177:9091/amodvis/static/image/84/de/4c/84de4c5d166dea9a96096ed49d649fa9.jpg"}
+	RoomBaseMap[3] = &RoomBaseModel{3, "room_3", "http://106.54.93.177:9091/amodvis/static/image/27/a0/a3/27a0a33aeac4e3b4b8b59a43edb34057.jpeg"}
+	RoomBaseMap[4] = &RoomBaseModel{4, "room_4", "http://106.54.93.177:9091/amodvis/static/image/27/a0/a3/27a0a33aeac4e3b4b8b59a43edb34057.jpeg"}
+	RoomBaseMap[5] = &RoomBaseModel{5, "room_5", "http://106.54.93.177:9091/amodvis/static/image/27/a0/a3/27a0a33aeac4e3b4b8b59a43edb34057.jpeg"}
+	RoomBaseMap[6] = &RoomBaseModel{6, "room_6", "http://106.54.93.177:9091/amodvis/static/image/84/de/4c/84de4c5d166dea9a96096ed49d649fa9.jpg"}
+	RoomBaseMap[7] = &RoomBaseModel{7, "room_7", "http://106.54.93.177:9091/amodvis/static/image/84/de/4c/84de4c5d166dea9a96096ed49d649fa9.jpg"}
+	RoomBaseMap[8] = &RoomBaseModel{8, "room_8", "http://106.54.93.177:9091/amodvis/static/image/27/a0/a3/27a0a33aeac4e3b4b8b59a43edb34057.jpeg"}
+	RoomBaseMap[9] = &RoomBaseModel{9, "room_9", "http://106.54.93.177:9091/amodvis/static/image/84/de/4c/84de4c5d166dea9a96096ed49d649fa9.jpg"}
+	RoomBaseMap[10] = &RoomBaseModel{10, "room_10", "http://106.54.93.177:9091/amodvis/static/image/84/de/4c/84de4c5d166dea9a96096ed49d649fa9.jpg"}
 }
 
 func (u OrmUserModel) GetByID(roomID int32) *RoomBaseModel {
